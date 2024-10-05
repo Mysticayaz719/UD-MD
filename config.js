7f4fa1d0f27a1061f89f4066d1c2c9de5de87b19
Ɗᴀʀᴋ Ɗєνιʟ 👑🇵🇰🖤 <umarofficial404@gmail.com>
2024-10-05 17:13:25 +0500

    Uᴍᴀʀ Uᴅ ♥

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+OWNER_NUMBER: '923165123719@s.whatsapp.net',
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/IfaDdJf.jpeg",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi UD-MD Is Online Now 💻\n*💻 Owner* - UMAR REHMAN\n\n*💻 Owner Number* -923165123719\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: "true",
+ELEVENLABS_API_KEY: 'sk_4669df1ac80b92a39528954725c4ff916fbfc7e3a4d75b22',
+};
